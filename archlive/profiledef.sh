0639a387b35105ae8fc5b56a161b4a152481b25d
piotr <nwg.piotr@gmail.com>
2023-09-11 01:28:48 +0200

    customize profiledef; enable NetworkManager.service

--- archlive/profiledef.sh
+++ archlive/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="archlinux-nwg"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_publisher="Piotr Miller <https://nwg-piotr.github.io/nwg-shell>"
+iso_application="Arch Linux Live/Rescue CD w/ nwg-shell installer"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
